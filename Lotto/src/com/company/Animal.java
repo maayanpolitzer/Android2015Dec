8486de3002389efa2ae22eed462133c14bb08597
maayanpolitzer <maayanpolitzer@Maayans-MacBook-Air.local>
2015-12-17 10:38:43 +0200

    Lotto

---
+++ Lotto/src/com/company/Animal.java
@@ -0,0 +1,14 @@
+package com.company;
+
+/**
+ * Created by hackeru on 2015-12-16.
+ */
+public class Animal {
+
+    private final int NUM_OF_LEGS = 4;
+
+    public int nunmberOfLegs(){
+        return 4;
+    }
+
+}
